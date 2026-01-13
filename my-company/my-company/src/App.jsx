be1bace8add6c17c59057dd3156d3911b0190517
solomon amaechi <solomornejohn@gmail.com>
2026-01-13 10:39:56 +0100

    simple company website with react

--- my-company/my-company/src/App.jsx
+++ my-company/my-company/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -18,8 +17,6 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-
-      <Footer />
     </BrowserRouter>
   );
 }
