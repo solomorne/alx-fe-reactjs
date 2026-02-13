33f30d27d7247f12c847e9d5ccba919f79aa7031
solomon amaechi <solomornejohn@gmail.com>
2026-02-13 17:00:42 +0100

    Setting up react app for recipe with tailwindcss

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Recipe Sharing Platform 🍲
+      </h1>
+    </div>
+  )
+}
+
+export default App
